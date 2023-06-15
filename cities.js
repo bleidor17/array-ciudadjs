5b8192ee520e55745c6b371c52694344f440b2d3
bladimir benjumea <bleidor17@gmail.com>
2023-06-14 20:57:10 -0500

    carga del proyecto a github

---
+++ cities.js
@@ -0,0 +1,41 @@
+var cities =[
+{
+ " name":"armenia",
+ "code":01
+
+},
+{
+    " name":"Barranquilla",
+    "code":02
+},
+{
+    " name":"bogota",
+    "code":03
+ },
+ {
+    " name":"cartagena",
+    "code":04
+   
+ },
+ {
+    " name":"medellin",
+    "code":05
+   
+},
+{
+    " name":"valledupar",
+    "code":06
+   
+ },
+
+
+
+
+
+
+
+
+
+
+
+];
